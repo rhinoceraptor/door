6ab9bf5b1ec5fdb23a626380d0d58f9d9ba95556
Jack Lewis <rhinoceraptor@gmail.com>
2014-09-25 20:23:39 -0400

    moved web into it's own repository

--- server.js
+++
@@ -1,4 +0,0 @@
-var express = require(express);
-var app = express();
-
-app.listen(10042);
--- server/server.js
+++ server/server.js
@@ -1,3 +1,10 @@
-var express = require(express);
+/*
+ * Set up the REST interface
+ */
+var express = require('express');
 var app = express();
+app.listen(10042);
 
+/*
+ * Register REST events
+ */
